2d384c1476ba3714342ec07bdf35dc3049260211
Matthias Vermeulen <47433305+vermmatt@users.noreply.github.com>
2020-09-22 18:59:44 +0200

    Update Switch.java

--- Week 1/Switch/Switch.java
+++ Week 1/Switch/Switch.java
@@ -7,12 +7,11 @@ class Switch {
     int num1 = keyboard.nextInt();
     System.out.print("Enter second number: ");  
     int num2 = keyboard.nextInt();
-    int temp1 = num1;
-    int temp2 = num2;
-    num2 = temp1;
-    num1 = temp2;
-    System.out.println("The first number is: " + num1);  
-    System.out.println("The second number is: " + num2);  
-
+    int temp = num1;
+    System.out.println("first: " + num1 + " second: " + num2 + " temporary: " + temp);
+    num1 = num2;
+    System.out.println("first: " + num1 + " second: " + num2 + " temporary: " + temp);
+    num2 = temp;
+    System.out.println("first: " + num1 + " second: " + num2 + " temporary: " + temp);
   }
 }
