afb35efd4c45059e9ea3a45399fad1c5c77a02ce
Matthias Vermeulen <47433305+vermmatt@users.noreply.github.com>
2020-10-03 09:57:23 +0200

    Create Digits2.java

---
+++ Week 2/03_Digits2/Digits2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Digits2 {
+    public static void main(String[] args) {
+        int MIN = 1000;
+        int MAX = 10000;
+        int sum = 0;
+        int number;
+        Scanner keyboard = new Scanner(System.in);
+
+
+        while(true) {
+            System.out.print("Geef een geheel getal: ");
+            number = keyboard.nextInt();
+            if(number > 0){
+                while(number > 0) {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                System.out.println("De som van de afzonderlijke cijfers is " + sum);
+                sum = 0;
+                } else if (number == -1) {
+                    return;
+                } else {
+                System.out.println("Getal valt niet binnnen de range.");
+            }
+        }
+
+    }
+}
