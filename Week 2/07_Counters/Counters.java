6815ef8d6e1d7ccaa7435fe0d20cf8cd4ddfe52b
Matthias Vermeulen <47433305+vermmatt@users.noreply.github.com>
2020-10-03 14:10:30 +0200

    Create Counters.java

---
+++ Week 2/07_Counters/Counters.java
@@ -0,0 +1,8 @@
+public class Counters {
+    public static void main(String[] args){
+        int AANTAL = 10;
+        for(int i = 1; i <= AANTAL; i++){
+            System.out.println(i + " - " + (AANTAL+1-i));
+        }
+    }
+}
