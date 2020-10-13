60526e910c108970666312a77d6258617e461e4a
Matthias Vermeulen <47433305+vermmatt@users.noreply.github.com>
2020-10-13 17:34:00 +0200

    Create Numbers.java

---
+++ Week 3/Numbers/Numbers.java
@@ -0,0 +1,7 @@
+public class Numbers {
+    public static void main(String[] args) {
+        for (int i = -10; i <= 10; i++) {
+            System.out.print((i>0) ? "+" + i + " " : i + " ");
+        }
+    }
+}
